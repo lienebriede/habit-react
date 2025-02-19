3e8d550b710b2248ac6620b2a8d3b73f2b032a32
Liene Briede <lienesjaunkundze@gmail.com>
2025-02-19 11:09:25 +0200

    Set up with CI template & Bootstrap

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Start here!</Button>
+    </div>
+  );
+}
+
+export default App;
